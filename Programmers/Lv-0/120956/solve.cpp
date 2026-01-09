b6eca5bc0290ff63ad8c5373b925db68f827034e
whitelinkblaze <whitelinkblaze@gmail.com>
2026-01-09 14:21:14 +0900

    change code 120956

--- Programmers/Lv-0/120956/solve.cpp
+++ Programmers/Lv-0/120956/solve.cpp
@@ -9,12 +9,12 @@ int solution(vector<string> babbling) {
     int targetIndex = 0;
     int targetLength = 0;
     bool check = true;
-    for(int i = 0; i < babbling.size(); i++)
+    for(string babb: babbling) 
     {
         check = true;
-        for(int j = 0; j < babbling[i].length(); j++)
+        for(int j = 0; check && j < babb.length(); j++)
         {
-            switch(babbling[i][j])
+            switch(babb[j])
             {
                 case 'a':
                     targetIndex = 0;
@@ -33,14 +33,10 @@ int solution(vector<string> babbling) {
             
             targetLength = targetString[targetIndex].length();
             
-            if(babbling[i].length() - j >= targetLength && 
-                targetString[targetIndex].compare(babbling[i].substr(j,targetLength))==0)
+            if(targetString[targetIndex].compare(babb.substr(j,targetLength))==0)
                 j+=targetLength-1;
             else
-            {
-                j = babbling[i].length();
                 check = false;
-            }
         }
         if (check) answer++;
     }
